54d0c29c8327483b9ad436ec6b670d3b6a7ce49f
Jason Katonica <katonica@us.ibm.com>
2025-03-03 15:39:11 -0500

    Set default FIPS profile
    When a user does not specify a specific FIPS profile name via `-Dsemeru.customprofile` we should default to the `NSS.140-2` profile instead of setting our profile to NULL. Setting our profile to NULL causes a NPE later while processing the profile settings.
    
    Signed-off-by: Jason Katonica <katonica@us.ibm.com>

--- closed/src/java.base/share/classes/openj9/internal/security/RestrictedSecurity.java
+++ closed/src/java.base/share/classes/openj9/internal/security/RestrictedSecurity.java
@@ -138,9 +138,14 @@ public final class RestrictedSecurity {
             }
         }
 
-        // If user has specified a profile, use that
-        selectedProfile = System.getProperty("semeru.customprofile");
-        userSetProfile = selectedProfile != null;
+        // If user has specified a profile, use that instead of the default.
+        String userSpecifiedProfile = System.getProperty("semeru.customprofile");
+        if (userSpecifiedProfile != null) {
+            selectedProfile = userSpecifiedProfile;
+            userSetProfile = true;
+        } else {
+            userSetProfile = false;
+        }
 
         // Check if FIPS is supported on this platform without explicitly setting a profile.
         if (userEnabledFIPS && !isFIPSSupported && !userSetProfile) {
